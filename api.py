e0671b1155911b18732f2809b84d9e5f40debe39
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2026-03-02 21:37:34 +0530

    fix

--- api.py
+++ api.py
@@ -16,8 +16,7 @@ from huggingface_hub import hf_hub_download, login
 
 hf_token = os.getenv("HF_TOKEN")
 
-if hf_token:
-    login(token=hf_token)
+
     
 os.environ["HF_HUB_DISABLE_WARNING"] = "1"
 
